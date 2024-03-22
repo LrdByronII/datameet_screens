b5f07a3c07264f82dbecd842de7f58cba92c4ac2
Byron <byron@datameet.com>
2024-03-22 14:53:46 +1100

    de rendered the other pages tvs acting up

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import "./App.css";
 import { Box, Grid, GridItem } from "@chakra-ui/react";
-import MembershipPage from "./componenets/MembershipPage/MembershipPage";
 import MerchPage from "./componenets/MerchPage/MerchPage";
-import NetworkPage from "./componenets/NetworkPage/NetworkPage";
 
 function App() {
   return (
@@ -12,8 +10,6 @@ function App() {
         <GridItem area="aside"></GridItem>
         <GridItem area="main">
           <Box>
-            <NetworkPage />
-            <MembershipPage />
             <MerchPage />
           </Box>
         </GridItem>
